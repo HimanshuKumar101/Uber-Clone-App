fba28706237b7a949d27ecb9a3c0089aead17f37
himanshu101 <himanshukumar.connect@gmail.com>
2025-03-09 21:31:05 +0530

    create user

--- Backend/app.js
+++ Backend/app.js
@@ -4,21 +4,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 
 connectToDb();
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
 
-app.get('/', (req,res) => {
-    
+app.get('/', (req, res) => {   
     res.send('Hello World');
  });
 
- module.exports = app;
+app.use('/users', userRoutes);
+module.exports = app;
 
 
