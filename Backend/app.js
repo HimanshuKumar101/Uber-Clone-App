0fb1d35e0500dc82cc30104ec60fecd29f6ea9f9
himanshu101 <himanshukumar.connect@gmail.com>
2025-04-18 21:36:56 +0530

    backend complete

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const cookieParser = require('cookie-parser');
+const express = require('express');
 const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
-
 
 app.use(cors());
 app.use(express.json());
@@ -18,19 +19,17 @@ app.use(cookieParser());
 
 
 
-
-
-app.get('/', (req, res) => {   
+app.get('/', (req, res) => {
     res.send('Hello World');
- });
+});
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
-
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 
 
 
 module.exports = app;
-
 
